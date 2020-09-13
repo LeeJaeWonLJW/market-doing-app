e12b9b5abcc371dadc667e393c143aa1a7d1e995
Jay Jaewon Lee <jaewon@jaewon.me>
2020-09-14 08:34:53 +0900

    Add files via upload

---
+++ Customer/app/src/main/java/univ/customer/MainActivity.java
@@ -0,0 +1,14 @@
+package univ.customer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
